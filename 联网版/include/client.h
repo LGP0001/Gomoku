a2e48c94dad629dd12363db7ee018a453efc1676
Li Guangpu <lguangpu001@gmail.com>
2023-10-08 10:35:18 +0800

    无语了

--- 联网版/include/client.h
+++ 联网版/include/client.h
@@ -1,11 +1,7 @@
 # ifndef COMMON_H
 # define COMMON_H
 
-<<<<<<< HEAD
-# include <ncurses.h>
-=======
 # include <ncursesw/ncurses.h>
->>>>>>> 14d2211 (修改文件目录)
 # include <locale.h>
 # include <stddef.h>
 # include <stdbool.h>
@@ -22,21 +18,12 @@
 
 bool check_win(char chessboard[][COL], int x, int y, char color) ;                 // 判断胜负
 bool perform_register(int sockfd, const char *username, const char *password) ;
-<<<<<<< HEAD
-bool perform_login(int sockfd, const char *username, const char *password) ;
-bool perform_delete(int sockfd, const char *username, const char *password) ;
-=======
 bool perform_login(int sockfd, const char *username, const char *password, bool *is_admin) ;
 bool perform_delete(int sockfd, const char *username, const char *password) ;
 void init_chessman(int type, wchar_t *tableline) ;
 void init_chessboard(int row, int col, wchar_t chessboard[][COL]) ;
->>>>>>> 14d2211 (修改文件目录)
 
 
 
 
-<<<<<<< HEAD
 # endif
-=======
-# endif
->>>>>>> 14d2211 (修改文件目录)
